3cf3871fa81936a689ccd79111057951e743ab37
Anthony Thudium <anthonythudium@Anthony-Thudium.local>
2018-12-04 20:28:19 -0800

    First Commit

---
+++ MyClass.java
@@ -0,0 +1,23 @@
+package newpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyClass {
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		
+		String baseUrl = "https://www.google.com";
+		driver.get(baseUrl);
+		
+		driver.findElement(By.name("q")).sendKeys("Obama");
+		
+		Thread.sleep(5000);
+		
+		driver.findElement(By.name("btnK")).click();
+		driver.close();
+		
+	    }
+}
---
+++ PG2.java
@@ -0,0 +1,23 @@
+package newpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PG2 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "https://www.facebook.com";
+		String tagName = "";
+		
+		driver.get(baseUrl);
+		tagName = driver.findElement(By.id("email")).getTagName();
+		System.out.println(tagName);
+		
+		Thread.sleep(Long.MAX_VALUE);
+		driver.close();
+		System.exit(0);
+	}
+}
