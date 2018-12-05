e7037df16947703bde5070234b529e5e0619ecd5
Anthony Thudium <anthonythudium@Anthony-Thudium.local>
2018-12-05 15:11:26 -0800

    swithTo(), alert(), getText()

---
+++ MyClass_2.java
@@ -0,0 +1,23 @@
+package newpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyClass_2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		
+		String alertMessage = "";
+		
+		driver.get("http://jsbin.com/usidix/1");
+		driver.findElement(By.cssSelector("input[value=\"Go!\"]")).click();
+		alertMessage = driver.switchTo().alert().getText();
+		driver.switchTo().alert().accept();
+		
+		System.out.println(alertMessage);
+		Thread.sleep(5000);
+		driver.quit();
+	}
+}
